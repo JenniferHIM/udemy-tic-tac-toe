55bccee02b8edafd2d0a40b3581564c92fd699ab
Evgeniya Obernikhina <jenniferhim1201@gmail.com>
2023-12-14 12:05:39 +0100

    add basic files

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+            <span id="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player </span>
+            <span id="player-symbol">O</span>
+          </li>
+        </ol>
+        GAMEBOARD
+      </div>
+      LOG
+    </main>
+  );
+}
+
+export default App;
